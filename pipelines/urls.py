34fe2355f40296165e4048c1cf20b672d781bf7f
Eduard Martinez <martinez.bernal@gmail.com>
2022-04-13 00:17:13 +0200

    lol

--- pipelines/urls.py
+++ pipelines/urls.py
@@ -1,8 +1,9 @@
 from .views import *
 from django.urls import path, include
 
+app_name = 'pipelines'
 
 urlpatterns = [
-    
-    
+    path('', PipelineList.as_view(), name='home'),
+    path('new/', PipelineCreate.as_view(), name='new'),
 ]
--- pipelines/views.py
+++ pipelines/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from .models import Pipeline
 
-# Create your views here.
+class PipelineList(ListView):
+	model = Pipeline
+
+class PipelineCreate(CreateView):
+	model = Pipeline
