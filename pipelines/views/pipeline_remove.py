75d4b953ad0af8d198083e89737299703ed6f379
Eduard Martinez <martinez.bernal@gmail.com>
2022-04-22 21:08:51 +0200

    aa

--- pipelines/views/pipeline_remove.py
+++ pipelines/views/pipeline_remove.py
@@ -6,11 +6,13 @@ from .pipeline_filtered_mixin import PipelineFilteredMixin
 
 class PipelineRemove(PipelineFilteredMixin, DeleteView):
 
+    def get(self, request):
+         return HttpResponseRedirect(self.get_success_url())
+
     def form_valid(self, form):
-        success_url = self.get_success_url()
         self.object.environments.remove(self.current_environment)
         messages.success(self.request, "%s was removed successfully" % self.object)
-        return HttpResponseRedirect(success_url)
+        return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
         return reverse('pipelines:home', kwargs={'environment': self.current_environment.slug })
