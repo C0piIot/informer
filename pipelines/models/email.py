77cab61cb9b096aec591d5af854375c13b482aff
Eduard Martinez <martinez.bernal@gmail.com>
2022-04-27 21:56:01 +0200

    WIP

--- pipelines/models/email.py
+++ pipelines/models/email.py
@@ -5,9 +5,9 @@ from .send_channel import SendChannel
 class Email(SendChannel):
 	email_channel = models.ForeignKey('configuration.EmailChannel', on_delete=models.CASCADE)
 	subject = models.CharField(_('subject'), max_length=200)
-	html_body = models.TextField(_('html body'))
-	text_body = models.TextField(_('text_body'))
-	autogenerate_text = models.BooleanField(_('autogenerate text'), default=True)
+	html_body = models.TextField(_('html body message'))
+	text_body = models.TextField(_('plain text message'), help_text=_("Text used on clients that don't support html emails"))
+	autogenerate_text = models.BooleanField(_('autogenerate text'), default=True, help_text="Generate text automatically from html template")
 
 	class Meta:
 		verbose_name = _('email template')
