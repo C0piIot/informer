89c5c3ce6ab82d1fd465679799d25944f19ef672
Eduard Martinez <martinez.bernal@gmail.com>
2022-05-09 23:23:06 +0200

    lo

--- contacts/views/contact_list.py
+++ contacts/views/contact_list.py
@@ -4,12 +4,12 @@ from django.conf import settings
 
 class ContactList(TemplateView):
 
+	template_name = 'contacts/contact_list.html'
+
 	def get_context_data(self, **kwargs):
 		context_data = super().get_context_data(**kwargs)
-
 		contact_storage = import_string(settings.CONTACT_STORAGE)
-		
-
-		print(contact_storage.get_contacts(0))
-		
+		context_data.update({
+			'contacts': contact_storage.get_contacts(0)
+		})
 		return context_data
