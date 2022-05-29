85bc2967dd1e4f1ecf4fbeab330594302800601d
Eduard Martinez <martinez.bernal@gmail.com>
2022-05-29 12:20:59 +0200

    missing

---
+++ pipelines/models/pipeline_run.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class PipelineRun(models.Model):
+    event = models.CharField(_('event'), max_length=200)
+    contact_key = models.CharField(_('contact key'), max_length=100,)
+    event_payload = models.JSONField(_('event payload'), default=dict)
+    pipeline_data = models.JSONField(_('pipeline data'), default=dict)
+
+
+    def next_step(current_step):
+        pass
