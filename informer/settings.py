a0714fa36711ffa7280a4ecba548ac75e9eca65b
Eduard Martínez <edu@gotrendier.com>
2022-10-10 08:35:57 +0200

    joe

--- informer/settings.py
+++ informer/settings.py
@@ -84,10 +84,7 @@ AUTH_USER_MODEL = 'configuration.User'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL', default='sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite3'))
 }
 
 # Password validation
