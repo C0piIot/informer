3340e4364b0507bedc1954f0a93362e9e9b55999
Eduard Martinez <martinez.bernal@gmail.com>
2022-10-12 19:11:23 +0200

    todos

--- pipelines/views/pipeline_set_revision.py
+++ pipelines/views/pipeline_set_revision.py
@@ -7,6 +7,7 @@ from django.db import transaction
 from .pipeline_filtered_mixin import PipelineFilteredMixin
 from django.shortcuts import get_object_or_404
 from django.views.generic.detail import SingleObjectMixin
+from configuration.models import Environment
 import uuid
 
 from pipelines.models import Pipeline
@@ -20,7 +21,8 @@ class PipelineSetRevision(PipelineFilteredMixin, SingleObjectMixin, View):
         revision = None
         with transaction.atomic():
             revision = get_object_or_404(Pipeline.objects.filter(id=pipeline.id), pk=request.POST.get('revision', None))
-            self.current_environment.pipelines.remove(pipeline)
-            self.current_environment.pipelines.add(revision)
+            environment = get_object_or_404(Environment.objects.filter(account=self.current_account), pk=request.POST.get('environment', None))
+            environment.pipelines.remove(pipeline)
+            environment.pipelines.add(revision)
             messages.success(self.request, _("Revision set succesfully"))
         return HttpResponseRedirect(reverse('pipelines:history', kwargs={'id': pipeline.id, 'environment': self.current_environment.slug }))
