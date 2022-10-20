32efc7984c965d09a9b15a1d7d118381dcf0f8be
Eduard Martinez <martinez.bernal@gmail.com>
2022-10-20 23:22:22 +0200

    moe

--- flows/forms/test_form.py
+++ flows/forms/test_form.py
@@ -2,6 +2,8 @@ from django.forms import ModelForm
 from flows.models import FlowRun
 from django.conf import settings
 from django.utils.module_loading import import_string
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class TestForm(ModelForm):
@@ -14,9 +16,20 @@ class TestForm(ModelForm):
         self.environment = kwargs.pop('environment')
         super().__init__(**kwargs)
 
+    def clean(self):
+        cleaned_data = super().clean()
+        cleaned_data['contact'] = import_string(settings.CONTACT_STORAGE).get_contact(self.environment, cleaned_data['contact_key'])
+        if not cleaned_data['contact']:
+            self.add_error('contact_key', ValidationError(
+                _("Can't find contact with key %(contact_key)s"),
+                code='invalid_contact_key',
+                params={'contact_key': cleaned_data['contact_key']},
+            ))
+
     def save(self, commit=True):
         flow_run = super().save(commit=False)
         flow_run.flow_revision = self.flow
+        flow_run.contact = self.cleaned_data['contact']
         if commit:
             import_string(settings.FLOW_RUN_STORAGE).save_flow_run(self.environment, flow_run)
         return flow_run
