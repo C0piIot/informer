1fdf71fe7f05c4819d9efebcec18dbb3be0ea605
Eduard Martínez <edu@gotrendier.com>
2022-10-25 08:45:17 +0200

    MAS SWAP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ ! -z "$SWAP" ]]; then fallocate -l 64M swapfile && chmod 600 swapfile && mkswap swapfile && swapon swapfile && ls -hla; fi;
+if [[ ! -z "$SWAP" ]]; then fallocate -l 128M swapfile && chmod 600 swapfile && mkswap swapfile && swapon swapfile && ls -hla; fi;
 
 python manage.py migrate
 sqlite3 /usr/src/app/db.sqlite3 < /usr/src/app/pragma.sql
 
-redis-server --maxmemory 32M &
+redis-server --maxmemory 16M &
 python manage.py rundramatiq &
 uwsgi /usr/src/app/uwsgi.ini
