1acd6f9680ff6954b9f936a0059039ea283a110f
Eduard Martínez <edu@gotrendier.com>
2022-10-31 17:14:11 +0100

    last firebase fixes

--- accounts/models/push_channel.py
+++ accounts/models/push_channel.py
@@ -9,23 +9,33 @@ from firebase_admin import messaging
 class PushChannel(Channel):
     firebase_credentials = models.JSONField(_("Firebase credentials"), default=dict)
 
+    def firebase_app_name(self):
+        return "firebase-app-%d" % self.account.pk
+
     def clean_fields(self, exclude=None):
         super().clean_fields(exclude=exclude)
         if not exclude or not 'firebase_credentials' in exclude:
             if self.firebase_credentials:
                 try:
-                    self.init_firebase()
+                    firebase_admin.delete_app(firebase_admin.get_app(name=self.firebase_app_name()))
+                except:
+                    pass
+                try:
+                    self.get_firebase()
                 except ValueError as err:
                     raise ValidationError({
                         'firebase_credentials': _("The firebase credentials data doen't seems to be correct: %s" % err)
                     })
 
 
-    def init_firebase(self):
-        return firebase_admin.initialize_app(firebase_admin.credentials.Certificate(self.firebase_credentials))
+    def get_firebase(self):
+        try:
+            return firebase_admin.get_app(name=self.firebase_app_name())
+        except ValueError:    
+            return firebase_admin.initialize_app(firebase_admin.credentials.Certificate(self.firebase_credentials), name=self.firebase_app_name())
 
     def send_push(self, title, body, url, tokens):
-        firebase = self.init_firebase()
+        firebase = self.get_firebase()
         
         response = messaging.send_multicast(
             messaging.MulticastMessage(
