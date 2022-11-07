1deef8074a3c82aa0cf423cd472f23fc6f2022d4
Eduard Martinez <martinez.bernal@gmail.com>
2022-11-07 19:46:48 +0100

    LOL

---
+++ flows/static/flows/test.js
@@ -0,0 +1,18 @@
+let timer = null;
+const form = document.getElementById('form-test-flow'),
+	curlExcample = document.getElementById('id_curl_example'),
+	apiUrl = curlExcample.dataset.urlEndpoint,
+	privateKey = curlExcample.dataset.privateKey,
+	event = curlExcample.dataset.event,
+	update = async () => {
+		const body = new URLSearchParams({
+			"event" : event,
+			"contact_key" : form.elements['contact_key'].value,
+			"event_payload" : form.elements['event_payload'].value,
+		});
+		curlExcample.value = `curl '${apiUrl}' -H 'Authorization: Bearer ${privateKey}' -H 'Content-Type: application/x-www-form-urlencoded' --data-raw '${body}'`;
+	};
+
+update();
+form.addEventListener("keyup", update);
+form.addEventListener('reset', update);
