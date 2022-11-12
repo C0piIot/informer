37b5e300b264c45fec5e601ab04e077681dcd030
Eduard Martinez <martinez.bernal@gmail.com>
2022-11-12 12:56:01 +0100

    Pagination

--- contacts/views/contact_list.py
+++ contacts/views/contact_list.py
@@ -4,12 +4,17 @@ from django.conf import settings
 from accounts.views import CurrentEnvironmentMixin
 
 class ContactList(CurrentEnvironmentMixin, TemplateView):
-
+	amount = 10
 	template_name = 'contacts/contact_list.html'
 
 	def get_context_data(self, **kwargs):
 		context_data = super().get_context_data(**kwargs)
 		context_data.update({
-			'object_list': import_string(settings.CONTACT_STORAGE).get_contacts(self.current_environment),
+			'object_list': import_string(settings.CONTACT_STORAGE).get_contacts(
+				self.current_environment,
+				amount=self.amount,
+				start_key=self.request.GET.get('start_key', None)
+			),
+			'amount' : self.amount
 		})
 		return context_data
