266f17cd1b843adbd4db52bd4cefc610351dacf4
Eduard Martinez <martinez.bernal@gmail.com>
2022-11-14 20:27:57 +0100

    test

--- accounts/tests.py
+++ accounts/tests.py
@@ -9,7 +9,17 @@ class EnvironmentTestCase(TransactionTestCase):
 	def setUp(self):
 		pass
 
-	def testEnvironmentCreate(self):
-		self.client.force_login(get_user_model().objects.first())
-		self.client.get(reverse('accounts:environment_list'))
+	def testEnvironmentCrud(self):
+		with self.settings(ALLOWED_HOSTS=('example.com',)):
+			self.client.force_login(get_user_model().objects.first())
+			
+			response = self.client.get(reverse('accounts:environment_list'), HTTP_HOST='example.com')
+			self.assertNotContains(response, 'data-bs-target="#delete')
 
+			response = self.client.post(reverse('accounts:environment_create'), 
+				{'name': 'test_environment'}, HTTP_HOST='example.com')
+			self.assertEqual(response.status_code, 302)
+			self.assertEqual(response.headers['location'], reverse('accounts:environment_list'))
+
+			response = self.client.get(reverse('accounts:environment_list'), HTTP_HOST='example.com')
+			self.assertContains(response, 'data-bs-target="#delete', count=1)
