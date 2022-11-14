e43a24f82761c7e66ea3cc857117ae46fbee30f8
Eduard Martinez <martinez.bernal@gmail.com>
2022-11-14 20:45:51 +0100

    ole

--- accounts/tests.py
+++ accounts/tests.py
@@ -13,13 +13,30 @@ class EnvironmentTestCase(TransactionTestCase):
 		with self.settings(ALLOWED_HOSTS=('example.com',)):
 			self.client.force_login(get_user_model().objects.first())
 			
-			response = self.client.get(reverse('accounts:environment_list'), HTTP_HOST='example.com')
-			self.assertNotContains(response, 'data-bs-target="#delete')
+			self.assertNotContains(
+				self.client.get(
+					reverse('accounts:environment_list'),
+					HTTP_HOST='example.com'
+				),
+				'data-bs-target="#delete'
+			)
 
-			response = self.client.post(reverse('accounts:environment_create'), 
-				{'name': 'test_environment'}, HTTP_HOST='example.com')
-			self.assertEqual(response.status_code, 302)
-			self.assertEqual(response.headers['location'], reverse('accounts:environment_list'))
+			self.assertRedirects(
+				self.client.post(
+					reverse('accounts:environment_create'), 
+					{'name': 'test_environment'},
+					HTTP_HOST='example.com'
+				),
+				reverse('accounts:environment_list')
+			)
 
-			response = self.client.get(reverse('accounts:environment_list'), HTTP_HOST='example.com')
-			self.assertContains(response, 'data-bs-target="#delete', count=1)
+			self.assertContains(
+				self.client.get(
+					reverse('accounts:environment_list'),
+					HTTP_HOST='example.com'
+				), 
+				'data-bs-target="#delete',
+				count=1
+			)
+
+			
