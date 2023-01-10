fd2566bd14e3d098daab8f5795d96c423e86c4c4
Eduard Martinez <martinez.bernal@gmail.com>
2023-01-10 21:13:39 +0000

    apns tokens in contacts

--- accounts/forms/push_contact_form.py
+++ accounts/forms/push_contact_form.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 import re
 
-class FCMTokensWidget(forms.Textarea):
+class TokensWidget(forms.Textarea):
 
     def __init__(self, attrs=None):
         default_attrs = {"spellcheck": "false"}
@@ -16,18 +16,25 @@ class FCMTokensWidget(forms.Textarea):
             return None
         return "\n".join(value) if type(value) == list else value
 
+fcm_token_validation = re.compile("^[0-9A-Za-z_:-]{100,500}$")
+apns_token_validation = re.compile("^[0-9a-fA-F]{1,500}$")
 
-class FCMTokensField(forms.Field):
-    widget = FCMTokensWidget
-    fcm_token_validation = re.compile("^[0-9A-Za-z_:-]{100,500}$")
+class MultipleTokensField(forms.Field):
+    widget = TokensWidget
+    help_text = _('Enter one registration token per line')
+    
+    def __init__(self, *args, **kwargs):
+        self.token_validation = kwargs.pop('token_validation')
+        super().__init__(*args, **kwargs)
 
     def to_python(self, value):
         lines = [s.strip() for s in value.splitlines()] if value else []
         for line in lines:
-            if not self.fcm_token_validation.match(line):
+            if not self.token_validation.match(line):
                 raise ValidationError(_('Invalid tokens'))
         return lines
 
 
 class PushContactForm(forms.Form):
-    fcm_tokens = FCMTokensField(label=_('FCM Tokens'), required=False, help_text=_('Enter one registration token per line'))
+    fcm_tokens = MultipleTokensField(label=_('FCM Tokens'), required=False, token_validation=fcm_token_validation)
+    apns_tokens = MultipleTokensField(label=_('APNS Tokens'), required=False, token_validation=apns_token_validation)
