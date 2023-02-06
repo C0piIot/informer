bb567e65c5692b279d810b7ee7feb1f89ef0199d
Eduard Martinez <martinez.bernal@gmail.com>
2023-02-06 22:49:59 +0000

    pylint fixes

--- inbox/models/inbox_entry.py
+++ inbox/models/inbox_entry.py
@@ -1,8 +1,7 @@
+from uuid import uuid4
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.sites.models import Site
 from contacts.models import RelatedContactModel
-from uuid import uuid4
 
 class InboxEntry(RelatedContactModel):
     key = models.UUIDField(_('key'), default=uuid4, primary_key=True, editable=False)
@@ -12,7 +11,8 @@ class InboxEntry(RelatedContactModel):
     url = models.URLField(_('url'), blank=True, default='')
     image = models.URLField(_('image'), blank=True, default='')
     read = models.DateTimeField(_('read'), blank=True, null=True)
-    entry_data = models.JSONField(_('entry data'), default=dict, help_text=_('Additional data for custom implementations'), blank=True)
+    entry_data = models.JSONField(_('entry data'), default=dict, 
+        help_text=_('Additional data for custom implementations'), blank=True)
 
     def __str__(self):
         return self.message
