8583d927826411efa1be611822dd028d12170a91
Eduard Martinez <martinez.bernal@gmail.com>
2023-02-09 22:36:17 +0000

    more tests

--- contacts/tests/functional/test_contacts.py
+++ contacts/tests/functional/test_contacts.py
@@ -4,15 +4,13 @@ from contacts.apps import ContactsConfig
 from accounts.models import Environment
 
 class EnvironmentsTestCase(TransactionTestCase):
-    fixtures = ["users.json", "environments.json"]
+    fixtures = ["users.json", "environments.json", "channels.json", "email_channels.json", "push_channels.json"]
 
     def setUp(self):
         self.environment = Environment.objects.first()
         self.client.force_login(self.environment.site.users.first())
 
     def testEnvironmentCrud(self):
-
-        
 
         with self.settings(ALLOWED_HOSTS=("example.com",)):
             with self.settings(CONTACT_STORAGE=ContactsConfig.DEFAULT_SETTINGS['CONTACT_STORAGE']):
@@ -21,4 +19,25 @@ class EnvironmentsTestCase(TransactionTestCase):
                 )
 
                 self.assertContains(response, 'No contacts found')
-            
+
+                response = self.client.post(
+                    reverse("contacts:contact_create", kwargs={'environment': self.environment.slug}),
+                    {
+                        "name": "example name",
+                        "key": 123,
+                        "auth_key" : "abc",
+                        "channels" : [1 ,2],
+                        "contact_data" : "{\"foo\":\"bar\"}",
+                        "channel-1-email" : "abc@example.com",
+                        "channel-2-fcm_tokens" : "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                    },
+                    HTTP_HOST="example.com",
+                    follow=True
+                )
+
+                self.assertRedirects(
+                    response,
+                    reverse("contacts:list", kwargs={'environment': self.environment.slug}),
+                )
+
+                self.assertNotContains(response, 'No contacts found')
