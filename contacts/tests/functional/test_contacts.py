cd68a3917ee952a560623636e733bfc7b2673021
Eduard Martinez <martinez.bernal@gmail.com>
2023-02-10 18:38:18 +0000

    test 100 delete

--- contacts/tests/functional/test_contacts.py
+++ contacts/tests/functional/test_contacts.py
@@ -67,6 +67,15 @@ class EnvironmentsTestCase(TransactionTestCase):
                 self.assertContains(response, 'example name updated')
 
 
+                self.assertRedirects(
+                    self.client.get(
+                        reverse("contacts:contact_remove", kwargs={'environment': self.environment.slug, 'key': 123 }),
+                        HTTP_HOST="example.com"
+                    ),
+                    reverse("contacts:list", kwargs={'environment': self.environment.slug}),
+                )
+
+
                 response = self.client.post(
                     reverse("contacts:contact_remove", kwargs={'environment': self.environment.slug, 'key': 123 }),
                     HTTP_HOST="example.com",
