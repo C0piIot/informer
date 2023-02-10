f1830d721e661f5365feabccae8c35768231fa87
Eduard Martinez <martinez.bernal@gmail.com>
2023-02-10 18:30:08 +0000

    more coverage

--- contacts/tests/functional/test_contacts.py
+++ contacts/tests/functional/test_contacts.py
@@ -41,3 +41,44 @@ class EnvironmentsTestCase(TransactionTestCase):
                 )
 
                 self.assertNotContains(response, 'No contacts found')
+                self.assertContains(response, 'example name')
+
+
+                response = self.client.post(
+                    reverse("contacts:contact_update", kwargs={'environment': self.environment.slug, 'key': 123 }),
+                    {
+                        "name": "example name updated",
+                        "key": 123,
+                        "auth_key" : "abc",
+                        "channels" : [1 ,2],
+                        "contact_data" : "{\"foo\":\"bar\"}",
+                        "channel-1-email" : "abc@example.com",
+                        "channel-2-fcm_tokens" : "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                    },
+                    HTTP_HOST="example.com",
+                    follow=True
+                )
+
+                self.assertRedirects(
+                    response,
+                    reverse("contacts:list", kwargs={'environment': self.environment.slug}),
+                )
+
+                self.assertContains(response, 'example name updated')
+
+
+                response = self.client.post(
+                    reverse("contacts:contact_remove", kwargs={'environment': self.environment.slug, 'key': 123 }),
+                    HTTP_HOST="example.com",
+                    follow=True
+                )
+
+                self.assertRedirects(
+                    response,
+                    reverse("contacts:list", kwargs={'environment': self.environment.slug}),
+                )
+
+                self.assertNotContains(response, 'example name updated')
+                self.assertContains(response, 'Contact was removed successfully')
+                self.assertContains(response, 'No contacts found')
+                
