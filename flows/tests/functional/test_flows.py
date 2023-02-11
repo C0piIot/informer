01c9a14d4fe4795c7aafbc8657751fbd8e67fae1
Eduard Martinez <martinez.bernal@gmail.com>
2023-02-11 19:53:38 +0000

    wip crud tests

--- flows/tests/functional/test_flows.py
+++ flows/tests/functional/test_flows.py
@@ -1,7 +1,8 @@
 from django.test import TransactionTestCase
 from django.urls import reverse
+from django.contrib.contenttypes.models import ContentType
 from accounts.models import Environment
-from flows.models import Flow
+from flows.models import Flow, Delay, Email, Group, Push, Webhook
 
 class FlowsTestCase(TransactionTestCase):
     fixtures = ["users.json", "environments.json", "channels.json", "email_channels.json", "push_channels.json"]
@@ -53,7 +54,40 @@ class FlowsTestCase(TransactionTestCase):
             response = self.client.post(
                 reverse("flows:remove", kwargs={'environment': self.environment.slug,  'id': flow.id}),
                 HTTP_HOST="example.com",
-                follow=True
+    
             )
             
 
+    def testFlowStepsCrud(self):
+
+        flow = Flow.objects.create(name="test flow", trigger="test", site=self.environment.site, user=self.environment.site.users.first())
+        flow.environments.add(self.environment)
+        with self.settings(ALLOWED_HOSTS=("example.com",)):
+            self.assertContains(
+                self.client.get(
+                    reverse("flows:edit", kwargs={'environment': self.environment.slug,  'id': flow.id}),
+                    HTTP_HOST="example.com"
+                ),
+                "This flow has no steps yet."
+            )
+
+            response = self.client.post(
+                reverse(
+                    "flows:step_create", 
+                    kwargs={'environment': self.environment.slug,  'id': flow.id, 'content_type_id': ContentType.objects.get_for_model(Delay).pk },
+
+                ),
+                { "time": 30 },
+                HTTP_HOST="example.com",
+                follow=True
+            )
+
+            self.assertRedirects(
+                response,
+                reverse("flows:edit", kwargs={'environment': self.environment.slug, 'id': flow.id }),
+            )
+
+            self.assertContains(
+                response,
+                "Step was created successfully"
+            )
