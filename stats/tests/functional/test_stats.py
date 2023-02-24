3ff79f04024f63e3752cbd3d0171651cdc484d36
Eduard Martinez <martinez.bernal@gmail.com>
2023-02-24 18:58:03 +0000

    add missing file

---
+++ stats/tests/functional/test_stats.py
@@ -0,0 +1,20 @@
+from django.test import TransactionTestCase
+from django.urls import reverse
+from accounts.models import Environment
+
+class StatsTestCase(TransactionTestCase):
+    fixtures = ["users.json", "environments.json", "channels.json"]
+
+    def setUp(self):
+        self.environment = Environment.objects.first()
+        self.client.force_login(self.environment.site.users.first())
+
+    def testDashboard(self):
+        with self.settings(ALLOWED_HOSTS=("example.com",)):
+            self.assertContains(
+                self.client.get(
+                    reverse("stats:dashboard", kwargs={'environment': self.environment.slug }),
+                    HTTP_HOST="example.com"
+                ),
+                "Dashboard"
+            )
