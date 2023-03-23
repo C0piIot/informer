725312635351e4f622f0a556e56fc310ca86f5f3
Eduard Martinez <martinez.bernal@gmail.com>
2023-03-23 20:35:47 +0000

    setting rollbar version

--- informer/context_processors.py
+++ informer/context_processors.py
@@ -2,9 +2,11 @@ from django.conf import settings
 
 
 def rollbar_settings(request):
+
     return {
         "ROLLBAR_CLIENT_TOKEN": settings.ROLLBAR_CLIENT_TOKEN,
-        "ROLLBAR_ENVIRONMENT": settings.ROLLBAR["environment"]
+        "ROLLBAR_ENVIRONMENT": settings.ROLLBAR["environment"],
+        "ROLLBAR_CODE_VERSION": settings.BUILD_VERSION.split('.')[-1] #Last element is the commit sha1
     }
 
 
