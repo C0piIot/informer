98ba37c0a699ec8eafe009e8fa6e8eed0efc0d25
Eduard Martinez <martinez.bernal@gmail.com>
2023-04-01 11:28:56 +0000

    update curl example

--- flows/static/flows/test.js
+++ flows/static/flows/test.js
@@ -1,16 +1,16 @@
 const form = document.getElementById('form-test-flow'),
-	curlExcample = document.getElementById('id_curl_example'),
-	apiUrl = curlExcample.dataset.urlEndpoint,
-	privateKey = curlExcample.dataset.privateKey,
-	event = curlExcample.dataset.event,
-	update = async () => {
-		const body = new URLSearchParams({
-			"event" : event,
-			"contact_key" : form.elements['contact_key'].value,
-			"event_payload" : form.elements['event_payload'].value,
-		});
-		curlExcample.value = `curl '${apiUrl}' -H 'Authorization: Bearer ${privateKey}' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept:application/json; version=1' --data-raw '${body}'`;
-	};
+	input = document.getElementById('id_curl_example'),
+	update = () => 
+		input.value = 
+			`curl '${input.dataset.urlEndpoint}' -H 'Authorization: Bearer ${input.dataset.privateKey}' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept:application/json; version=1' --data-raw '${new URLSearchParams({
+				"event" : input.dataset.event,
+				"contact_key" : form.elements['contact_key'].value,
+				"event_payload" : form.elements['event_payload'].value,
+			})}'`;
+
 update();
 form.addEventListener("keyup", update);
 form.addEventListener('reset', update);
+
+document.getElementById('curl-example-copy')
+	.addEventListener('click', () => navigator.clipboard.writeText(input.value));
