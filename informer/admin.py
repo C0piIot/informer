38f242cde7b6e0c1fb91312ba18a5f099882e308
Eduard Martinez <martinez.bernal@gmail.com>
2023-04-07 15:27:36 +0000

    wip

--- informer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-
-class InformerAdmin(admin.AdminSite):
-    site_header = "Informer Admin"
-    site_title = "Informer"
-    index_title = "Informer administration"
